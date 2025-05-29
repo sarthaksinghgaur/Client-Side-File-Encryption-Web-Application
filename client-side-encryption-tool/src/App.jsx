783e26bbfaf858e81feca2048988ad4d36697fc6
Sarthak Singh Gaur <s7s5g4@gmail.com>
2025-05-29 07:25:31 +0530

    Setup Node

---
+++ client-side-encryption-tool/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Client-Side File Encryption Tool</h1>
+      <p>Let’s build something awesome!</p>
+    </div>
+  );
+}
+
+export default App;
